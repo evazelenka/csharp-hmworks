66366a349d2f6c11e99ea6624e89dd57f73a8e95
zelenka <eva.zelya@gmail.com>
2024-05-12 12:21:06 +0300

    add Task1

---
+++ Homeworks/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿void multipleTo( int num)
+{
+    if (num % 7 == 0 && num % 23 == 0)
+    {
+        Console.WriteLine($"{num} is multiple to 7 and to 23.");
+    } else
+    {
+        Console.WriteLine($"{num} is not multiple to 7 and to 23.");
+    }
+}
+
+Console.WriteLine("Input number");
+int number = Convert.ToInt32(Console.ReadLine());
+multipleTo(number);

990c3fe3c390a41a811b59eaad84c66166f381da
zelenka <eva.zelya@gmail.com>
2024-05-12 12:35:02 +0300

    add Task2

---
+++ Homeworks/Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿void coordinatesOfPoint(int a, int b) 
+{
+    if (a != 0 && b != 0)
+    {
+        if(a > 0 && b > 0)
+        {
+            Console.WriteLine(1);
+        }
+        if(a < 0 && b > 0)
+        {
+            Console.WriteLine(2);
+        }if(a < 0 && b < 0)
+        {
+            Console.WriteLine(3);
+        }if(a > 0 && b < 0)
+        {
+            Console.WriteLine(4);
+        }
+    }else
+    {
+        Console.WriteLine("wrong coordinates");
+    }
+}
+
+Console.WriteLine("Input x");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input y");
+int y = Convert.ToInt32(Console.ReadLine());
+coordinatesOfPoint(x, y);
+

62be79603513ba72a3c4994668c7cd530959531d
zelenka <eva.zelya@gmail.com>
2024-05-12 13:22:10 +0300

    add Task4

---
+++ Homeworks/Task4/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите натуральное число N: ");  //input 113
+int N = Convert.ToInt32(Console.ReadLine());
+if (N < 10)                                  //113 > 10
+{
+Console.WriteLine(N);
+}
+else
+{
+while (N > 0)                               //113 > 0    //11 > 0   //1 > 0
+{
+int currentDigit = N % 10;               //output 3     //output 1   //output 1
+N /= 10;                                 //output 11    //output 1   //output 0
+if (N > 0)                               //11 > 0        // 1 > 0    //0 = 0
+{
+Console.Write(currentDigit + ",");        //output 3,   //output 1,  
+}
+else
+{
+Console.WriteLine(currentDigit);                                     //output 1
+}
+}
+}

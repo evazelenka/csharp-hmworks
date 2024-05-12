160790f64273cdc51a4bd0e1c0b2b93f21f49631
zelenka <eva.zelya@gmail.com>
2024-05-12 13:23:46 +0300

    add Sem2

---
+++ Seminars/Sem2/Program.cs
@@ -0,0 +1,71 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// //Console.WriteLine("Hello, World!");
+
+// int numtoquad(int num)
+// {
+//     return num * num;
+// }
+
+// Console.WriteLine("Input number");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine(numtoquad(number));
+
+
+// void del_sec_num(int num)
+// {
+//     if(num >= 100 && num < 1000)
+//     {
+//         Console.WriteLine($"{num / 100*10 + num % 10}");
+//     } else
+//     {
+//         Console.Write("this number is not three digit.");
+//     }
+// }
+
+// Console.WriteLine("Input number");
+// int number = Convert.ToInt32(Console.ReadLine());
+// del_sec_num(number);
+
+
+// int secondToThird(int num)
+// {
+//     if(num >= 100 && num < 1000)
+//     {
+//         int ed = num % 10;
+//         int dec = (num / 10) % 10;
+//         int result = 1;
+//         for (int i = 0; i < ed; i++)
+//         {
+//            result = result * dec;
+//         }
+//         return result;
+//     } else
+//     {
+//         return 0;
+//     }
+// }
+
+// Console.WriteLine("Input number");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(secondToThird(number));
+
+
+// void firstToSecond(int num1, int num2)
+// {
+//     if (num1 % num2 == 0)
+//     {
+//         Console.WriteLine($"{num1} is multiple of {num2}.");
+//     }else
+//     {
+//         Console.WriteLine($"{num1 % num2}");
+//     }
+// }
+
+// Console.WriteLine("Input first number");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second number");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// firstToSecond(number1, number2);
+
+

d9ae7d0dad759f1bf75a60c8455dc285e359935c
zelenka <eva.zelya@gmail.com>
2024-05-12 12:54:19 +0300

    add Task3

---
+++ Homeworks/Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿void maxNum(int x)
+{
+    if(x > 9 && x < 100)
+    {
+        if((x / 10) > (x % 10))
+        {
+            Console.WriteLine($"{x / 10}");
+        }
+        if((x / 10) < (x % 10))
+        {
+            Console.WriteLine($"{x % 10}");
+        }
+        if((x / 10) == (x % 10))
+        {
+            Console.WriteLine("equal");
+        }
+    }else
+    {
+        Console.WriteLine($"{x} is not allowed");
+    }
+}
+Console.WriteLine("input number [10, 99]");
+int num = Convert.ToInt32(Console.ReadLine());
+maxNum(num);

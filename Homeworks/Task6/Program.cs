8f95ca8443faf73654cd39c6890ecb091b37a09c
zelenka <eva.zelya@gmail.com>
2024-05-13 23:42:53 +0300

    add homeworks:task6

---
+++ Homeworks/Task6/Program.cs
@@ -0,0 +1,39 @@
+﻿//  Задайте массив заполненный случайными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+int[] CreateRandomArray(int size)
+{
+    int[] array = new int [size];
+    for(int i = 0; i < size; i++)
+    {
+        array [i] = new Random().Next(100, 1000);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void ShowEvenElements(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array [i] % 2 == 0)
+        {
+            count++;
+        }else
+        count+= 0;
+    }
+    Console.WriteLine(count);
+}
+
+Console.WriteLine("Input size");
+int size = Convert.ToInt32 (Console.ReadLine());
+int[] arr = CreateRandomArray(size);
+PrintArray(arr);
+ShowEvenElements(arr);

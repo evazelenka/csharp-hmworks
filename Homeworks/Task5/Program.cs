e0e1b43a911d0c267f354eea61676e0e71b0daa7
zelenka <eva.zelya@gmail.com>
2024-05-13 16:55:12 +0300

    add homeworks:task5

---
+++ Homeworks/Task5/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая бесконечно запрашивает целые числа с консоли. 
+// Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
+
+void Endless (int y)
+{
+    int cnt = 1;
+    int sum = 0;
+    while (cnt % 2 != 0) 
+    {
+        if(y > 9)
+    {
+         while(y > 0)
+        {
+            sum = sum + y % 10;
+            y = y / 10;
+        }
+        cnt = sum;
+    } else
+    {
+        cnt = y;
+    }
+    if (cnt % 2 != 0)
+    {
+        Console.WriteLine("Input Number");
+        y = Convert.ToInt32(Console.ReadLine());
+        cnt = 1;
+        sum = 0;
+    }
+    }
+}
+Console.WriteLine("Input Number");
+int x = Convert.ToInt32(Console.ReadLine());
+Endless (x);
